395f4aab5d7195804f2c909ef14333978e5968d6
wooody92 <pkower4@naver.com>
2020-04-20 14:08:02 +0900

    [chore] 개발환경 설정 #2
    의존성 주입 및 개발환경 설정

---
+++ BE/sidedish-11/src/main/java/dev/codesquad/java/sidedish11/Sidedish11Application.java
@@ -0,0 +1,13 @@
+package dev.codesquad.java.sidedish11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sidedish11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sidedish11Application.class, args);
+	}
+
+}
