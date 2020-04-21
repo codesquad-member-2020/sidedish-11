4b626d796a71c2ebc0f6554293fb97404759da3b
wooody92 <pkower4@naver.com>
2020-04-21 18:10:26 +0900

    [feature] main, soup, side controller 생성 #15
    - controller package : main, side, soup 를 담당하는 컨트롤러 생성했습니다.

---
+++ BE/sidedish11/src/main/java/dev/codesquad/java/sidedish11/controller/ApiMainController.java
@@ -0,0 +1,13 @@
+package dev.codesquad.java.sidedish11.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/main")
+public class ApiMainController {
+    private Logger logger = LoggerFactory.getLogger(ApiMainController.class);
+
+}
---
+++ BE/sidedish11/src/main/java/dev/codesquad/java/sidedish11/controller/ApiSideController.java
@@ -0,0 +1,13 @@
+package dev.codesquad.java.sidedish11.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/side")
+public class ApiSideController {
+    private Logger logger = LoggerFactory.getLogger(ApiSideController.class);
+
+}
