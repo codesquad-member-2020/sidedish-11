0d6cae86edc97a14c0b5503bc0d86ca8763db166
wooody92 <pkower4@naver.com>
2020-04-21 14:52:14 +0900

    [feature] data.sql 추가 #7
    - data.sql : 임시 data 입력 하였습니다.

---
+++ BE/sidedish11/src/main/resources/data.sql
@@ -0,0 +1,1 @@
+INSERT INTO item(detail_hash, image, alt, title, description, n_price, p_price) VALUES ('SD001', 'http://image.url', '치아버터 빵 100g', '치아버터 빵 100g', '커피랑 먹으면 꿀맛', '3,000원', null);
--- BE/sidedish11/src/main/resources/schema.sql
+++ BE/sidedish11/src/main/resources/schema.sql
@@ -2,11 +2,11 @@ DROP TABLE IF EXISTS item;
 
 CREATE TABLE item (
   id int primary key auto_increment,
-  detail_hash varchar (64),
-  image varchar (64),
-  alt varchar (64),
-  title varchar (64),
-  description varchar (64),
-  n_price varchar (64),
-  p_price varchar (64)
+  detail_hash varchar (32),
+  image varchar (256),
+  alt varchar (128),
+  title varchar (128),
+  description varchar (256),
+  n_price varchar (32),
+  p_price varchar (32)
 );
