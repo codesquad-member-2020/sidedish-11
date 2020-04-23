33054c51d1d7a12f616542db411b1f79fb7b91cb
dev-Lena <keunnalee@gmail.com>
2020-04-24 01:24:00 +0900

    [refactor] NetworkManager 클래스 내의 enum Utility 클래스로 이동 #24

--- iOS/Sidedish/Sidedish/Utility.swift
+++ iOS/Sidedish/Sidedish/Utility.swift
@@ -23,3 +23,11 @@ enum EventCase: String, CustomStringConvertible {
     
     case SpecialPrice = "이벤트특가", FreeGift = "사은품증정", SoldOut = "품절", LaunchingPrice = "론칭특가"
 }
+
+enum EndPoints {
+    static let main = "https://h3rb9c0ugl.execute-api.ap-northeast-2.amazonaws.com/develop/baminchan/main"
+    static let side = "https://h3rb9c0ugl.execute-api.ap-northeast-2.amazonaws.com/develop/baminchan/side"
+    static let soup = "https://h3rb9c0ugl.execute-api.ap-northeast-2.amazonaws.com/develop/baminchan/soup"
+    static let detail = "https://h3rb9c0ugl.execute-api.ap-northeast-2.amazonaws.com/develop/baminchan/detail/"
+    static let placeholder = "https://byline.network/wp-content/uploads/2018/11/%EB%B0%B0%EB%AF%BC%EC%B0%AC-%EB%A1%9C%EA%B3%A0.jpg"
+}
