2fb6798e788e725aa1a548f04bb4288f1cf321da
dev-Lena <keunnalee@gmail.com>
2020-04-23 21:59:47 +0900

    [chore] ViewController 클래스 생성 및 셋업  #12
    - TableView 셋업
    - Notification Observer 추가
    - 서버에 데이터 요청 메소드 추가

---
+++ iOS/Sidedish/Sidedish/Controller/MainViewController.swift
@@ -0,0 +1,49 @@
+//
+//  MainViewController.swift
+//  Sidedish
+//
+//  Created by Keunna Lee on 2020/04/20.
+//  Copyright © 2020 dev-Lena. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    @IBOutlet weak var mainSidedishTableView: UITableView!
+    private let cellNib = UINib(nibName: "SideDishTableViewCell", bundle: nil)
+    private let dataSource = SidedishDataSource()
+    private var useCase = SidedishUseCase()
+    private let delegate = SidedishTableViewDelegate()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupNotification()
+        setupTableView()
+        configureUseCase()
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self, name: NetworkManager.NetworkManagerNotification, object: nil)
+    }
+    
+    private func setupTableView(){
+        self.mainSidedishTableView.dataSource = dataSource
+        self.mainSidedishTableView.register(cellNib, forCellReuseIdentifier: SideDishTableViewCell.identifier)
+        self.mainSidedishTableView.delegate = delegate
+    }
+    
+    private func configureUseCase() {
+        useCase.bringSidedishMenu(with: NetworkManager())
+    }
+    
+    private func setupNotification() {
+        let notiCenter = NotificationCenter.default
+        notiCenter.addObserver(forName: NetworkManager.NetworkManagerNotification, object: nil, queue: .main) { noti in
+            guard let allMenu = noti.userInfo?["AllMenu"] as? [Int:[Sidedish]] else { return }
+            self.dataSource.allSidedishes = allMenu
+            self.mainSidedishTableView.reloadData()
+        }
+    }
+}
+
