d5603d406213ee723a05e2a9f8ad06f2c22b09fc
sgdg <seungdeng17@gmail.com>
2020-04-24 17:41:21 +0900

    [fix] 불필요한 콘솔로그 삭제

--- FE/client/src/components/main/item/Item.jsx
+++ FE/client/src/components/main/item/Item.jsx
@@ -5,7 +5,6 @@ const Item = ({ data }) => {
     const [isHover, setHover] = useState(false);
     const handleMouseEnter = () => setHover(true);
     const handleMouseLeave = () => setHover(false);
-    console.log(data.badge);
 
     return (
         <>
