b3f7b241fc10cbc23bec1ad55cebd9c8c3e050e3
dev-Lena <keunnalee@gmail.com>
2020-04-24 20:46:28 +0900

    [refactor] UseCase에서 파라미터로 url을 받아 데이터를 요청하도록 수정  #24

--- iOS/Sidedish/Sidedish/Model/SidedishUseCase.swift
+++ iOS/Sidedish/Sidedish/Model/SidedishUseCase.swift
@@ -12,45 +12,26 @@ class SidedishUseCase {
     
     static let SidedishUseCaseNotification = NSNotification.Name("NetworkManagerNotification")
 
-    func bringSidedishMenu(with manager: NetworkManagable) {
-        var allMenu: [Int:[Sidedish]] = [:]
-        
-        try? manager.getSidedishResource(from: EndPoints.main) { (data, error) in
+    func bringSidedishMenu(with manager: NetworkManagable, url: String) {
+        try? manager.getSidedishResource(from: url) { (data, error) in
             guard let data = data else { return }
-            let sidedishes = try? JSONDecoder().decode(SidedishInfo.self, from: data)
-            // API 완성 후 sidedshes에 있는 (카테고리 아이디-1)로 변경
-            allMenu[Category.Main] = sidedishes?.body
-            SidedishUseCase.sendNotification(allMenu: allMenu)
-        }
-        try? manager.getSidedishResource(from: EndPoints.soup) { (data, error) in
-            guard let data = data else { return }
-            let sidedishes = try? JSONDecoder().decode(SidedishInfo.self, from: data)
-            allMenu[Category.Soup] = sidedishes?.body
-            SidedishUseCase.sendNotification(allMenu: allMenu)
-
-        }
-        try? manager.getSidedishResource(from: EndPoints.side) { (data, error) in
-            guard let data = data else { return }
-            let sidedishes = try? JSONDecoder().decode(SidedishInfo.self, from: data)
-            allMenu[Category.Side] = sidedishes?.body
-            SidedishUseCase.sendNotification(allMenu: allMenu)
+            do {
+             let sidedishes = try JSONDecoder().decode(SidedishInfo.self, from: data)
+            SidedishUseCase.sendNotification(allMenu: sidedishes)
+            } catch {
+                print(error)
+            }
         }
     }
-    
+        
     func bringsidedishImage(with manager: NetworkManagable, imageURL: String, completed: @escaping (Data) -> ()) {
         try? manager.getSidedishResource(from: imageURL) { (data, error) in
             guard let image = data else { return }
             completed(image)
         }
     }
-    
-    enum Category {
-        static let Main = 0
-        static let Soup = 1
-        static let Side = 2
-    }
-    
-    static func sendNotification(allMenu: [Int:[Sidedish]]) {
+        
+    static func sendNotification(allMenu: SidedishInfo) {
         NotificationCenter.default.post(name: SidedishUseCase.SidedishUseCaseNotification, object: nil, userInfo: ["AllMenu":allMenu])
     }
 }
