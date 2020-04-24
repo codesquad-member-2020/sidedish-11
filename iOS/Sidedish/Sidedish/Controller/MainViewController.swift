268410b28be18b63b545ab3e0e31edb4da4aef75
dev-Lena <keunnalee@gmail.com>
2020-04-24 20:55:23 +0900

    [feat] 메인 페이지 ViewController 수정 #12
    - dataSource에 모델 전달
    - delegate에 모델 전달
    - 각 카테고리 별로 API에 데이터 신청
    - TableViewSection별로 업데이트 하도록 수정

--- iOS/Sidedish/Sidedish/Controller/MainViewController.swift
+++ iOS/Sidedish/Sidedish/Controller/MainViewController.swift
@@ -12,8 +12,11 @@ class MainViewController: UIViewController {
     
     @IBOutlet weak var mainSidedishTableView: UITableView!
     private let cellNib = UINib(nibName: "SideDishTableViewCell", bundle: nil)
+    private let headerNib = UINib(nibName: "SidedishTableViewHeader", bundle: nil)
     private let dataSource = SidedishDataSource()
     private var useCase = SidedishUseCase()
+    private let networkManager = NetworkManager()
+    private var allMenu = [Int:SidedishInfo]()
     private let delegate = SidedishTableViewDelegate()
     
     override func viewDidLoad() {
@@ -30,20 +33,28 @@ class MainViewController: UIViewController {
     private func setupTableView(){
         self.mainSidedishTableView.dataSource = dataSource
         self.mainSidedishTableView.register(cellNib, forCellReuseIdentifier: SideDishTableViewCell.identifier)
+        self.mainSidedishTableView.register(headerNib, forHeaderFooterViewReuseIdentifier: SidedishTableViewHeader.identifier)
         self.mainSidedishTableView.delegate = delegate
     }
     
     private func configureUseCase() {
-        useCase.bringSidedishMenu(with: NetworkManager())
+        useCase.bringSidedishMenu(with: MainMockNetworkSuccessStub(), url: "")
+        useCase.bringSidedishMenu(with: SoupMockNetworkSuccessStub(), url: "")
+        useCase.bringSidedishMenu(with: NetworkManager(), url: EndPoints.sideMockupURL)
     }
     
     private func setupNotification() {
         let notiCenter = NotificationCenter.default
         notiCenter.addObserver(forName: SidedishUseCase.SidedishUseCaseNotification, object: nil, queue: .main) { noti in
-            guard let allMenu = noti.userInfo?["AllMenu"] as? [Int:[Sidedish]] else { return }
-            self.dataSource.allSidedishes = allMenu
-            self.mainSidedishTableView.reloadData()
+            guard let allMenu = noti.userInfo?["AllMenu"] as? SidedishInfo else { return }
+            let categoryId = allMenu.id - 1
+            DispatchQueue.main.async {
+                self.allMenu[categoryId] = allMenu
+                self.dataSource.allSidedishes[categoryId] = allMenu.items
+                let indexSet: IndexSet = [categoryId]
+                self.delegate.headerModel = self.allMenu
+                self.mainSidedishTableView.reloadSections(indexSet, with: .automatic)
+            }
         }
     }
 }
-
