0db6b6ae0d7fa399d565209b7a3c1c7caf4e9fcb
dev-Lena <keunnalee@gmail.com>
2020-04-24 20:23:34 +0900

    [refactor] Utility클래스로 enum 이동 #12

--- iOS/Sidedish/Sidedish/Utility.swift
+++ iOS/Sidedish/Sidedish/Utility.swift
@@ -30,4 +30,12 @@ enum EndPoints {
     static let soup = "https://h3rb9c0ugl.execute-api.ap-northeast-2.amazonaws.com/develop/baminchan/soup"
     static let detail = "https://h3rb9c0ugl.execute-api.ap-northeast-2.amazonaws.com/develop/baminchan/detail/"
     static let placeholder = "https://byline.network/wp-content/uploads/2018/11/%EB%B0%B0%EB%AF%BC%EC%B0%AC-%EB%A1%9C%EA%B3%A0.jpg"
+    static let sideMockupURL = "http://www.mocky.io/v2/5ea24ec9310000d18f1ef037"
 }
+
+enum Category {
+    static let Main = 0
+    static let Soup = 1
+    static let Side = 2
+}
+
