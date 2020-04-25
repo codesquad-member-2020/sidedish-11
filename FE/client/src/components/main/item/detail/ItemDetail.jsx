d64c5d30314b4f3d41de0b5b25eb4e53f594211f
sgdg <seungdeng17@gmail.com>
2020-04-26 02:50:57 +0900

    [feat] thumb image 마우스 hover시 기본 이미지 변경 기능 추가 #35

--- FE/client/src/components/main/item/detail/ItemDetail.jsx
+++ FE/client/src/components/main/item/detail/ItemDetail.jsx
@@ -51,7 +51,9 @@ const useStyles = makeStyles({
 const ItemDetail = ({ itemKey, isDetailOpen, setDetailOpen }) => {
     const classes = useStyles();
     const [detailData, setDetailData] = useState(null);
+    const [currImage, setCurrImage] = useState(null);
     const handleClose = () => setDetailOpen(false);
+    const handleMouseEnter = ({ target }) => setCurrImage(<img src={target.src} alt={target.alt}></img>);
     const loading = useFetch(setDetailData, URL.DEV.DETAIL + itemKey);
 
     let data = null;
@@ -65,11 +67,11 @@ const ItemDetail = ({ itemKey, isDetailOpen, setDetailOpen }) => {
                     <DialogContent className={classes.detailWrap}>
                         <div className='detail-image-wrap'>
                             <div className='default-image'>
-                                <img src={data.top_image} alt={data.product_description}></img>
+                                {currImage ? currImage : <img src={data.top_image} alt={data.product_description}></img>}
                             </div>
                             <div className='thumb-images'>
-                                <img src={data.top_image} alt={data.product_description}></img>
-                                {data.thumb_images.map((imageURL, idx) => <img key={idx} src={imageURL} alt={data.product_description}></img>)}
+                                <img src={data.top_image} onMouseEnter={handleMouseEnter} alt={data.product_description}></img>
+                                {data.thumb_images.map((imageURL, idx) => <img onMouseEnter={handleMouseEnter} key={idx} src={imageURL} alt={data.product_description}></img>)}
                             </div>
                         </div>
                     </DialogContent>
