1593db070912c62b8c00b4856e2dc3979620f670
sgdg <seungdeng17@gmail.com>
2020-04-25 15:41:57 +0900

    [style] 슬라이드 아이템 클릭 시 아웃라인 생기지 않게 스타일 수정

--- FE/client/src/components/main/item/ItemList.jsx
+++ FE/client/src/components/main/item/ItemList.jsx
@@ -7,6 +7,8 @@ import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles({
     itemList: {
         cursor: 'pointer',
+        border: '0',
+        outline: '0',
     },
     slider: {
         '& .slick-dots': {
