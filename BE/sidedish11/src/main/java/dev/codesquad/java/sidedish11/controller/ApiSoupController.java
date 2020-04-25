06e39ef21f7f0d3765f1c09ba196614c55d6163b
wooody92 <pkower4@naver.com>
2020-04-25 15:45:47 +0900

    [feature] soup, side url path 추가 #15
    - ApiSideController.java : 동일 기능 side, soup 추가했습니다.

--- BE/sidedish11/src/main/java/dev/codesquad/java/sidedish11/controller/ApiSoupController.java
+++ BE/sidedish11/src/main/java/dev/codesquad/java/sidedish11/controller/ApiSoupController.java
@@ -1,7 +1,14 @@
 package dev.codesquad.java.sidedish11.controller;
 
+import dev.codesquad.java.sidedish11.dto.CategoryResponse;
+import dev.codesquad.java.sidedish11.service.CategoryService;
+import dev.codesquad.java.sidedish11.service.ItemService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +17,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class ApiSoupController {
     private Logger logger = LoggerFactory.getLogger(ApiSoupController.class);
 
+    @Autowired
+    private CategoryService categoryService;
+
+    @Autowired
+    private ItemService itemService;
+
+    @GetMapping("")
+    public ResponseEntity view() {
+        CategoryResponse categoryResponse = categoryService.getCategory(2L);
+        return ResponseEntity.ok(categoryResponse);
+    }
+
+    @GetMapping("/{hash}")
+    public ResponseEntity viewItem(@PathVariable String hash) {
+        return ResponseEntity.ok(itemService.getItem(2L, hash));
+    }
 }
