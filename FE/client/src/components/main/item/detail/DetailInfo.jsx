7e49bb9d356ba6c1f44ef117dd395f951bdfc8b9
sgdg <seungdeng17@gmail.com>
2020-04-27 00:40:16 +0900

    [feat] 상세 페이지 상품 정보 렌더링 기능 추가 #35

--- FE/client/src/components/main/item/detail/DetailInfo.jsx
+++ FE/client/src/components/main/item/detail/DetailInfo.jsx
@@ -1,5 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './style/DetailInfo.css';
+import ArrowDropUpIcon from '@material-ui/icons/ArrowDropUp';
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles({
@@ -8,15 +11,40 @@ const useStyles = makeStyles({
         height: 'auto',
         marginLeft: '30px',
     },
+    test: {
+        color: '#fff',
+        boxShadow: 'none',
+        marginTop: '20px',
+        width: '100%',
+        fontSize: '18px',
+        '&:hover': {
+            backgroundColor: '#27cfca',
+            borderColor: '#27cfca',
+            boxShadow: 'none',
+        },
+    }
 });
 
 const DetailInfo = ({ title, product_description, delivery_fee, delivery_info, point, prices, stock, numberComma }) => {
     const classes = useStyles();
+    const [count, setCount] = useState(1);
     const price = prices.reduce((acc, curr) => {
         if (acc === 0) acc = curr;
         else acc = curr && curr < acc ? curr : acc;
         return acc;
     }, 0);
+
+    const handleCount = (isCountUp) => {
+        let newCount = isCountUp ? count + 1 : count - 1;
+        if (newCount < 1) newCount = 1;
+        setCount(newCount);
+    };
+    const handleChange = ({ target }) => {
+        let newCount = parseInt(target.value);
+        if (typeof newCount !== 'number' || newCount === NaN || newCount < 0) return;
+        if (!target.value) newCount = 0;
+        setCount(newCount);
+    }
 
     return (
         <div className={classes.detailInfoWrap}>
@@ -38,6 +66,26 @@ const DetailInfo = ({ title, product_description, delivery_fee, delivery_info, p
                 {numberComma(price)}
                 <span className='detail-price-unit'>원</span>
             </div>
+            <div className='select-number'>
+                <div className='select-number-text'>수량</div>
+                <div className='select-input-wrap'>
+                    <input onChange={handleChange} className='select-input' type='text' maxLength='4' value={count}></input>
+                    <div className='select-input-btn-wrap'>
+                        <button onClick={() => { handleCount(true) }} className='select-input-up'>
+                            <ArrowDropUpIcon />
+                        </button>
+                        <button onClick={() => { handleCount(false) }} className='select-input-down'>
+                            <ArrowDropDownIcon />
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div className='amount-price'>
+                <div className='amount-price-text'>총 상품금액</div>
+                {numberComma(price * count)}
+                <span className='amount-price-unit'>원</span>
+            </div>
+            <Button className={classes.test} variant='contained' color='primary' children='담기' />
         </div>
     )
 }
