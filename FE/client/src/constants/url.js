a354bf22332dc58143f2f16b0682082c7ff713b4
sgdg <seungdeng17@gmail.com>
2020-04-27 21:59:59 +0900

    [chore] login api url 추가

--- FE/client/src/constants/url.js
+++ FE/client/src/constants/url.js
@@ -1,5 +1,6 @@
 const URL = {
     DEV: {
+        LOGIN: 'http://3.34.55.104/api/login',
         CATEGORY: 'http://3.34.55.104/api/category',
         DETAIL: 'http://3.34.55.104/api/detail/',
     }
