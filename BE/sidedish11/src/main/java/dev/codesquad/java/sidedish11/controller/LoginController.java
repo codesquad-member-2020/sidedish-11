371bd9d5fa5f5699b6a345d59ddf21bedf555fdb
wooody92 <pkower4@naver.com>
2020-04-28 00:58:41 +0900

    [feature] jason web token 테스트 #47
    - 토큰 값 헤더에 담아 전송합니다.

--- BE/sidedish11/src/main/java/dev/codesquad/java/sidedish11/controller/LoginController.java
+++ BE/sidedish11/src/main/java/dev/codesquad/java/sidedish11/controller/LoginController.java
@@ -1,5 +1,6 @@
 package dev.codesquad.java.sidedish11.controller;
 
+import dev.codesquad.java.sidedish11.jwt.TokenService;
 import dev.codesquad.java.sidedish11.oauth.Github;
 import dev.codesquad.java.sidedish11.oauth.GithubUser;
 import dev.codesquad.java.sidedish11.service.LoginService;
@@ -23,17 +24,24 @@ public class LoginController {
     @Autowired
     private LoginService loginService;
 
-//    @GetMapping("/login")
-//    public ResponseEntity login(HttpServletResponse response, HttpServletRequest request) {
-//        response.setHeader(HEADER_LOCATION, LOGIN_REQUEST_URL);
-//        return new ResponseEntity(HttpStatus.MOVED_PERMANENTLY);
-//    }
+    @Autowired
+    private TokenService tokenService;
+
+    @GetMapping("/login")
+    public ResponseEntity login(HttpServletResponse response) {
+        response.setHeader(HEADER_LOCATION, LOGIN_REQUEST_URL);
+        return new ResponseEntity(HttpStatus.MOVED_PERMANENTLY);
+    }
 
     @GetMapping("/callback")
     public ResponseEntity oauth(@PathParam("code") String code, HttpServletResponse response) {
         Github github = loginService.requestAccessToken(code);
         GithubUser githubUser = loginService.requestUserId(github.getAuthorization());
         response.setHeader(HEADER_LOCATION, SERVER_URL);
+
+        String jwt = tokenService.buildToken(githubUser);
+        response.setHeader(AUTHORIZATION, jwt);
+        response.setHeader(GITHUB_USER_ID, githubUser.getUserId());
         return new ResponseEntity(githubUser, HttpStatus.FOUND);
     }
 
