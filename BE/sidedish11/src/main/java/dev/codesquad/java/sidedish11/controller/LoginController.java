869f556dfda653555dcbb29b3eca90501f9c8ec8
wooody92 <pkower4@naver.com>
2020-04-27 23:32:00 +0900

    [fix] oauth 요청 시 api 방식 제거 후 링크 형식으로 변경 #47
    - 서버에서 github 리다이렉트 요청 시 cors error로 인한 롤백

--- BE/sidedish11/src/main/java/dev/codesquad/java/sidedish11/controller/LoginController.java
+++ BE/sidedish11/src/main/java/dev/codesquad/java/sidedish11/controller/LoginController.java
@@ -1,6 +1,5 @@
 package dev.codesquad.java.sidedish11.controller;
 
-import dev.codesquad.java.sidedish11.cors.SimpleCorsFilter;
 import dev.codesquad.java.sidedish11.oauth.Github;
 import dev.codesquad.java.sidedish11.oauth.GithubUser;
 import dev.codesquad.java.sidedish11.service.LoginService;
@@ -12,7 +11,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.websocket.server.PathParam;
 
@@ -25,19 +23,11 @@ public class LoginController {
     @Autowired
     private LoginService loginService;
 
-    @GetMapping("/login")
-    public ResponseEntity login(HttpServletResponse response, HttpServletRequest request) {
-
-        response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
-        response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Expose-Headers", "*, Authorization");
-        response.setHeader("Access-Control-Allow-Headers", "Origin, Content-Type, Accept, X-Requested-With, Authorization, Access-Control-Request-Method, Access-Control-Request-Headers");
-        response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, HEAD, OPTIONS, DELETE, PUT, PATCH");
-
-        response.setHeader(HEADER_LOCATION, LOGIN_REQUEST_URL);
-        return new ResponseEntity(HttpStatus.MOVED_PERMANENTLY);
-    }
+//    @GetMapping("/login")
+//    public ResponseEntity login(HttpServletResponse response, HttpServletRequest request) {
+//        response.setHeader(HEADER_LOCATION, LOGIN_REQUEST_URL);
+//        return new ResponseEntity(HttpStatus.MOVED_PERMANENTLY);
+//    }
 
     @GetMapping("/callback")
     public ResponseEntity oauth(@PathParam("code") String code, HttpServletResponse response) {
@@ -45,5 +35,10 @@ public class LoginController {
         GithubUser githubUser = loginService.requestUserId(github.getAuthorization());
         response.setHeader(HEADER_LOCATION, SERVER_URL);
         return new ResponseEntity(githubUser, HttpStatus.FOUND);
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "good";
     }
 }
