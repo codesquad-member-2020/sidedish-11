a7719607f5b8d26d6a49cfadfc2779b599a450ec
wooody92 <pkower4@naver.com>
2020-04-27 19:16:29 +0900

    [feature] 로그인 리다이렉트 테스트 #47
    - LoginController.java : redirect test 입니다.

--- BE/sidedish11/src/main/java/dev/codesquad/java/sidedish11/controller/LoginController.java
+++ BE/sidedish11/src/main/java/dev/codesquad/java/sidedish11/controller/LoginController.java
@@ -6,10 +6,12 @@ import dev.codesquad.java.sidedish11.service.LoginService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.websocket.server.PathParam;
 
 @RestController
@@ -19,10 +21,22 @@ public class LoginController {
     @Autowired
     private LoginService loginService;
 
+    GithubUser githubUser;
+
     @GetMapping("/login")
-    public ResponseEntity oauth(@PathParam("code") String code) {
+    public ResponseEntity oauth(@PathParam("code") String code, HttpServletResponse response) {
         Github github = loginService.requestAccessToken(code);
         GithubUser githubUser = loginService.requestUserId(github.getAuthorization());
-        return ResponseEntity.ok(githubUser);
+
+        String url = "/";
+        response.setHeader("Location", url);
+        this.githubUser = githubUser;
+        return new ResponseEntity(githubUser, HttpStatus.FOUND);
+        //return ResponseEntity.ok(githubUser);
+    }
+
+    @GetMapping("/")
+    public ResponseEntity home() {
+        return new ResponseEntity(githubUser, HttpStatus.OK);
     }
 }
